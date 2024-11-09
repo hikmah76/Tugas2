f43f8096b578edbb76db1f9a93f10bb2ded7b3a0
hikmah76 <nur.hikmah190504@gmail.com>
2024-11-09 10:59:10 +0700

    paw

---
+++ app.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+const app = express();
+const todoRoutes = require('./routes/tododb.js')
+const port = process.env.PORT;
+const db = require('./database/db');
+const expressLayouts = require('express-ejs-layouts')
+app.use(expressLayouts);
+
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index', {
+        layout : 'layouts/main-layout'
+    });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact', {
+        layout : 'layouts/main-layout'
+    });
+});
+
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
